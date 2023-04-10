ed7ab525c2f4d74e0b9aea54d778adb08a7b6a1b
cpot5620 <=cpot5620@naver.com>
2023-04-10 22:39:10 +0900

    (1일차) 연습문제0 해결

--- src/main/java/do_it/promblem0/Problem0.java
+++ src/main/java/do_it/promblem0/Problem0.java
@@ -1,2 +1,31 @@
-package do_it.promblem0;public class Problem0 {
+package do_it.promblem0;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem0 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
 }
